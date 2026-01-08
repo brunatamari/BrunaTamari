9d61eccd7a66e91818e947584cc420cc1929e05e
brunatamari <brunatamari@gmail.com>
2026-01-08 19:36:35 -0300

    Add files via upload

---
+++ projetos/script.js
@@ -0,0 +1,10 @@
+// Pequeno script JS para ativar quando o card entra na tela
+const cards = document.querySelectorAll('.card');
+window.addEventListener('scroll', () => {
+    cards.forEach(card => {
+        const top = card.getBoundingClientRect().top;
+        if(top < window.innerHeight - 50) {
+            card.classList.add('visible');
+        }
+    });
+});
